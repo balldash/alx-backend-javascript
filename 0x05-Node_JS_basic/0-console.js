61c0c3cefe2b884eec93a40c0ef11e00461d7b2d
Bislon Zulu <bislonzulu1@outlook.com>
2024-11-26 22:17:26 +0200

    task 1

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,13 @@
+/**
+ * Function logs a message to the console.
+ * @param {string} message - the message to log.
+ * @returns {voi}
+ */
+const displayMessage = (message) => console.log(message);
+
+/**
+ * Exports the displayMessage function.
+ * @module displayMessage
+ * @function
+ */
+module.exports = displayMessage;
