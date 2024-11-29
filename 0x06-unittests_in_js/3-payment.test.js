742e67af1d285186b125fb507f50b1eea14ed31b
Bislon Zulu <bislonzulu1@outlook.com>
2024-11-30 01:40:39 +0200

    fix task 3

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -5,11 +5,13 @@ const sendPaymentRequestToApi = require('./3-payment');
 
 describe('sendPaymentRequestToApi', () => {
   it('sendPaymentRequestToApi uses the calculateNumber method of Utils', () => {
-    const bigBrother = sinon.spy(Utils);
+    const spy = sinon.spy(Utils, 'calculateNumber');
 
     sendPaymentRequestToApi(100, 20);
-    expect(bigBrother.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
-    expect(bigBrother.calculateNumber.callCount).to.be.equal(1);
-    bigBrother.calculateNumber.restore();
+
+    expect(spy.calledWith('SUM', 100, 20)).to.be.true;
+    expect(spy.callCount).to.be.equal(1);
+
+    spy.restore();
   });
 });
